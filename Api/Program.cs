aa5e9804cbb1f2a02775bfbfd2c06c7fdf99e00c
Yeisson Gonzalez Guevara <Yeiduardo99@gmail.com>
2026-04-29 21:24:47 -0500

    Ajustes
    se ajusta swagger para usarlo en el deploy

--- Api/Program.cs
+++ Api/Program.cs
@@ -36,6 +36,13 @@ else
 {
     var port = Environment.GetEnvironmentVariable("PORT") ?? "8095";
     app.Urls.Add($"http://*:{port}");
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API v1");
+        c.RoutePrefix = string.Empty; 
+    });
+
 }
 
 app.UseHttpsRedirection();
